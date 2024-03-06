b21edb1180f8c4db3d02babfc78a23cd0308baab
Przemek Lisowski <przemek_lisowski@o2.pl>
2024-03-06 22:53:03 +0100

    add About section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// nawigacja
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
@@ -25,3 +26,63 @@ const handleNamItemsAnimation = () => {
 }
 
 navBtn.addEventListener('click', handleNav);
+
+// wjeżdzanie komponentów
+
+window.addEventListener('scroll', revealFromLeft);
+window.addEventListener('scroll', revealFromRight);
+window.addEventListener('scroll', revealFromDown);
+
+function revealFromLeft() {
+    const revealsLeft = document.querySelectorAll('.revealFromLeft');
+
+    for (let i = 0; i < revealsLeft.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = revealsLeft[i].getBoundingClientRect().top;
+        let revealPoint = 100;
+
+        if (revealTop < windowHeight - revealPoint) {
+            revealsLeft[i].classList.add('showFromLeft');
+        }
+        else {
+            revealsLeft[i].classList.remove('showFromLeft');
+        }
+    }
+}
+
+function revealFromRight() {
+    const revealsRight = document.querySelectorAll('.revealFromRight');
+
+    for (let i = 0; i < revealsRight.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = revealsRight[i].getBoundingClientRect().top;
+        let revealPoint = 100;
+
+        if (revealTop < windowHeight - revealPoint) {
+            revealsRight[i].classList.add('showFromRight');
+        }
+        else {
+            revealsRight[i].classList.remove('showFromRight');
+        }
+    }
+}
+
+function revealFromDown() {
+    const revealsDown = document.querySelectorAll('.revealFromDown');
+
+    for (let i = 0; i < revealsDown.length; i++) {
+
+        let windowHeight2 = window.innerHeight;
+        let revealTop2 = revealsDown[i].getBoundingClientRect().top;
+        let revealPoint2 = 100;
+
+        if (revealTop2 < windowHeight2 - revealPoint2) {
+            revealsDown[i].classList.add('showFromDown');
+        }
+        else {
+            revealsDown[i].classList.remove('showFromDown');
+        }
+    }
+}

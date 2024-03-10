6dffc00d011768a77cdd34984bbd18aaa6acfd76
Przemek Lisowski <przemek_lisowski@o2.pl>
2024-03-10 22:17:38 +0100

    add footer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@ const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const homeBtn = document.querySelector('.home-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
     nav.classList.toggle('nav--active')
@@ -26,6 +29,25 @@ const handleNamItemsAnimation = () => {
     })
 }
 
+// const handleObserver = () => {
+//     const currentSection = window.scrollY;
+
+//     allSections.forEach(section => {
+
+//         if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60)
+
+//     })
+// }
+
+
+// data w stopce
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
 homeBtn.addEventListener('click', handleNav);
 
